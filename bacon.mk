d4e8d6d1ac7c1a417c4872bfe9fad5bf8ec604f8
Raienryu97 <cherukupallip@gmail.com>
2016-11-02 13:51:12 -0100

    Revert "[DIRTY] bacon: disable remote codecs"
    This reverts commit ff64bf404177020d6f2aa3e5ec713a1a08dd47a8.
    
    Change-Id: Ibf5fb1915aaa229c09afdbeeb842c259ef08ff1e

--- bacon.mk
+++ bacon.mk
@@ -69,9 +69,6 @@ PRODUCT_PACKAGES += \
     camera.bacon \
     Snap
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.stagefright.codecremote=false
-
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
